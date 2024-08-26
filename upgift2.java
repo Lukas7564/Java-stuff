49ac0ec4b752426eff3ba146cce649670c2b6ba4
Lukas <lukas.a.karlsson@gmail.com>
2024-08-26 13:46:53 +0300

    Add files via upload

---
+++ upgift2.java
@@ -0,0 +1,16 @@
+
+public class upgift2 {
+
+    public static void main(String args[]) {
+    float num1 = 5;
+    float num2 = 7;
+        System.out.println(num1);
+        System.out.println(num2);
+        System.out.println(num1+num2);
+        System.out.println(num1-num2);
+        System.out.println(num1*num2);
+        System.out.println(num1/num2);
+
+    }
+
+}
