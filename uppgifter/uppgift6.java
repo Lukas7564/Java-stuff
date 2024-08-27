f6cbcc003066f327bbbd1819cbb3008955b4b7c0
Lukas <lukas.a.karlsson@gmail.com>
2024-08-27 13:24:38 +0300

    Add files via upload

---
+++ uppgifter/uppgift6.java
@@ -0,0 +1,13 @@
+
+public class upgift6 {
+
+    public static void main(String args[]) {
+    boolean lightOn = false;
+    boolean lightOf = true;
+    if (lightOn){
+        System.out.println("light is on");
+    }else {
+        System.out.println("light is of");
+    }
+    }
+}
