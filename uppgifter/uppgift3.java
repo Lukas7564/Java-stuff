1cd52fecc5f9fea397feb4c0088d8eefe09d4ee6
Lukas <lukas.a.karlsson@gmail.com>
2024-08-27 11:58:03 +0300

    Add files via upload

---
+++ uppgifter/uppgift3.java
@@ -0,0 +1,35 @@
+
+public class uppgift3 {
+
+    public static void main(String args[]) {
+    /*
+    int nber1 = 5;
+    Double nber2 = Double.valueOf(nber1);
+   
+        System.out.println(nber2);
+      */  
+    /*
+    float val1 = 6.74325f;
+    double val2 = Double.valueOf(val1);
+        System.out.println(val2);
+    */
+    
+    /*
+    
+    double rop = 6.345f;
+    int z = (int)rop;
+        System.out.println(z);
+        */
+    
+    /*
+    char car = '2';
+    int truck = (int)car;
+        System.out.println(truck);
+*/
+    int x = 5;
+    String y = Integer.toString(x);
+    byte bi = Byte.valueOf (y);
+    System.out.println(bi);
+    
+    }
+}
