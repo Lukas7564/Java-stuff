1643a15a1818d710cfc4ac88f126fb4cd2c637c3
Lukas <lukas.a.karlsson@gmail.com>
2024-08-29 12:05:56 +0300

    Add files via upload

---
+++ uppgifter/uppgift8.java
@@ -0,0 +1,15 @@
+
+public class upgift8 {
+
+    public static void main(String args[]) {
+        for(int in = 1; in<31;in++){
+            if(in==12){
+                break;
+        }
+            System.out.println(in);
+        }
+        System.out.println("Loop terminated early due to break statement.");
+
+
+    }
+}
