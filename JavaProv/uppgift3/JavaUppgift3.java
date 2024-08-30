31a5369b05252cb53dc5a5cd6f296d9023442357
Lukas <lukas.a.karlsson@gmail.com>
2024-08-30 12:56:34 +0300

    Update JavaUppgift3.java

--- JavaProv/uppgift3/JavaUppgift3.java
+++ JavaProv/uppgift3/JavaUppgift3.java
@@ -3,7 +3,7 @@ public class JavaUppgift3 {
 
     public static void main(String args[]) {
                 
-        for (int i = 0; i <= 5; i++) {
+        for (int i = 0; i < 5; i++) {
             System.out.println("Number:"  + i);
     
     }
